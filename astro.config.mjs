51aa648871d14c7cc877c164f4e2ec98d3cec90c
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-12-09 01:44:37 +0000

    setup domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.radenpioneer.work',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
