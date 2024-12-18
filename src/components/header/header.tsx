581b6a22a7cc855744b1593767a220f7bd285ac8
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-12-18 11:15:54 +0000

    rename brand colors

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import HeaderLogo from './logo'
 
 const Header: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <header className='fixed inset-x-0 top-0 z-[999] flex h-[64px] items-center justify-center border-b border-b-white bg-white/30 px-4 pt-4 pb-2 backdrop-blur-md'>
+    <header className='border-b-brand-background bg-brand-background/30 fixed inset-x-0 top-0 z-[999] flex h-[64px] items-center justify-center border-b px-4 pt-4 pb-2 backdrop-blur-md'>
       {children}
     </header>
   )
