cb47de07a26c1e6c59f454daccf65017c2cb6497
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-12-20 09:33:31 +0000

    revamp projects section

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,8 +3,8 @@ import HeaderLogo from './logo'
 
 const Header: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <header className='fixed inset-x-0 top-0 z-[999] h-[64px] sm:pt-2'>
-      <nav className='border-brand-background bg-brand-background/50 mx-auto flex max-w-screen-lg items-center justify-center border-b px-4 py-2 backdrop-blur-md sm:justify-start sm:rounded-full sm:border'>
+    <header className='fixed inset-x-0 top-0 z-[999] h-[64px] sm:px-4 sm:pt-2'>
+      <nav className='border-brand-background bg-brand-background/50 mx-auto flex max-w-screen-lg items-center justify-center border-b px-4 py-2 shadow backdrop-blur-md sm:justify-start sm:rounded-full sm:border'>
         {children}
       </nav>
     </header>
--- src/components/home/projects/projects.tsx
+++ src/components/home/projects/projects.tsx
@@ -9,16 +9,36 @@ const Project: FC<
       <div className='border-brand-background bg-brand-background/30 aspect-[16/10] overflow-hidden rounded-xl border shadow'>
         {children}
       </div>
-      <div className='border-brand-background bg-brand-background/30 flex flex-1 flex-col gap-2 rounded-xl border p-2 text-center shadow sm:py-4'>
-        <h3 className='text-xl font-bold'>{project.data.title}</h3>
-        <p className='italic'>{project.data.description}</p>
+      <div className='flex flex-1 flex-col gap-2'>
+        <h3 className='text-xl font-bold sm:text-2xl'>{project.data.title}</h3>
+        <p className='text-sm italic sm:text-base'>
+          {project.data.description}
+        </p>
         {project.data.tags && (
-          <div className='flex justify-center gap-1'>
-            {project.data.tags.map(({ tag }) => (
-              <span className='text-brand-primary border-brand-primary bg-brand-primary/5 rounded-full border px-2 py-1 text-xs uppercase'>
+          <div className='mt-auto flex gap-1'>
+            {project.data.tags.map(({ tag }, i) => (
+              <span
+                className='text-brand-primary border-brand-primary bg-brand-primary/5 rounded-full border px-4 py-1 text-xs uppercase'
+                key={i}
+              >
                 {tag.name}
               </span>
             ))}
+          </div>
+        )}
+        {(project.data.url || project.data.repo) && (
+          <div className='-mt-1 flex gap-1'>
+            {[project.data.url, project.data.repo]
+              .filter(Boolean)
+              .map((url, i) => (
+                <a
+                  className='text-brand-text border-brand-text bg-brand-text/5 rounded-full border px-4 py-1 text-xs font-bold uppercase'
+                  href={url}
+                  key={i}
+                >
+                  {i === 0 ? 'url' : 'repo'}
+                </a>
+              ))}
           </div>
         )}
       </div>
