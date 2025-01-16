abd0737a73ed1c555f6d5ef43b41b3d59493e591
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-01-16 11:20:34 +0000

    implement table features (1)

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,33 +1,58 @@
 import { type FC, useState } from 'react'
 import type { CollectionEntry } from 'astro:content'
 import {
+  type SortingFn,
+  type SortingState,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable
 } from '@tanstack/react-table'
+import { twMerge } from 'tailwind-merge'
 import URLIcon from '~icons/lucide/arrow-up-right'
 import GithubIcon from '~icons/lucide/github'
+import SortDescIcon from '~icons/lucide/arrow-down'
+import SortAscIcon from '~icons/lucide/arrow-up'
 
 type Project = CollectionEntry<'projects'>
 type ProjectsInput = {
   projects: Array<Project>
 }
 
+const sortStatusFn: SortingFn<Project> = (a, b) => {
+  const statusOrder = [
+    'completed',
+    'rejected',
+    'archived',
+    'on-progress',
+    'concept'
+  ]
+
+  return (
+    statusOrder.indexOf(a.original.data.status) -
+    statusOrder.indexOf(b.original.data.status)
+  )
+}
+
 const columnHelper = createColumnHelper<Project>()
 const columns = [
   // year
   columnHelper.accessor('data.date', {
+    id: 'year',
     header: 'Year',
-    cell: (p) => p.getValue()?.getFullYear()
+    cell: (p) => p.getValue()?.getFullYear(),
+    sortUndefined: 'last'
   }),
   // title
   columnHelper.accessor('data.title', {
+    id: 'title',
     header: 'Project Title',
     cell: (p) => <span className='font-bold'>{p.getValue()}</span>
   }),
   // status
   columnHelper.accessor('data.status', {
+    id: 'status',
     header: 'Status',
     cell: (p) => (
       <span
@@ -36,21 +61,28 @@ const columns = [
       >
         {p.getValue()}
       </span>
-    )
+    ),
+    sortingFn: sortStatusFn
   }),
   // made for?
   columnHelper.accessor('data.madeFor', {
     header: 'Made for',
-    cell: (p) => p.getValue()
+    cell: (p) => p.getValue(),
+    enableSorting: false
   }),
   // built with?
   columnHelper.accessor('data.tags', {
     header: 'Built with',
     cell: (p) => (
       <div className='*:border-brand-primary/75 *:bg-brand-primary/15 *:hover:bg-brand-primary/25 flex flex-wrap gap-1 *:rounded-full *:border *:px-2 *:py-0.5 *:text-sm *:transition-colors'>
-        {p.getValue()?.map(({ tag }) => <span role='button'>{tag.name}</span>)}
+        {p.getValue()?.map(({ tag }, _i) => (
+          <span role='button' key={_i}>
+            {tag.name}
+          </span>
+        ))}
       </div>
-    )
+    ),
+    enableSorting: false
   }),
   // urls
   columnHelper.display({
@@ -77,10 +109,17 @@ const columns = [
 
 const Projects: FC<ProjectsInput> = ({ projects }) => {
   const [data, _setData] = useState(() => [...projects])
+  const [sorting, setSorting] = useState<SortingState>([
+    { id: 'year', desc: true }
+  ])
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel: getCoreRowModel()
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: setSorting,
+    enableSortingRemoval: false,
+    state: { sorting }
   })
 
   return (
@@ -90,15 +129,28 @@ const Projects: FC<ProjectsInput> = ({ projects }) => {
           <tr key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
               <th
-                className='border-brand-primary/50 border p-1'
+                className='border-brand-primary/50 bg-brand-primary/10 border px-2 py-1'
                 key={header.id}
               >
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
+                {header.isPlaceholder ? null : (
+                  <div
+                    className={twMerge(
+                      header.column.getCanSort() && 'cursor-pointer',
+                      'flex items-center justify-center gap-0.5 text-center select-none'
+                    )}
+                    onClick={header.column.getToggleSortingHandler()}
+                    role='button'
+                  >
+                    {flexRender(
                       header.column.columnDef.header,
                       header.getContext()
                     )}
+                    {{
+                      asc: <SortAscIcon className='text-sm' />,
+                      desc: <SortDescIcon className='text-sm' />
+                    }[header.column.getIsSorted() as string] ?? null}
+                  </div>
+                )}
               </th>
             ))}
           </tr>
@@ -108,7 +160,10 @@ const Projects: FC<ProjectsInput> = ({ projects }) => {
         {table.getRowModel().rows.map((row) => (
           <tr key={row.id}>
             {row.getVisibleCells().map((cell) => (
-              <td className='border-brand-primary/50 border p-1' key={cell.id}>
+              <td
+                className='border-brand-primary/50 border px-2 py-1'
+                key={cell.id}
+              >
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </td>
             ))}
