d452b79431d49e1b500cac3a397145dd9e66cf55
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-19 16:53:48 +0000

    Add site configuration and integrate Keystatic for content management

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import cloudflare from '@astrojs/cloudflare'
-
 import tailwindcss from '@tailwindcss/vite'
+import react from '@astrojs/react'
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    '/sngrcreative': 'https://sngrcreative.pages.dev'
-  },
-
+  site: 'https://radenpioneer.work',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
   }),
-
+  integrations: [react(), keystatic()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD
+        ? {
+            'react-dom/server': 'react-dom/server.edge'
+          }
+        : {}
+    }
   }
 })
