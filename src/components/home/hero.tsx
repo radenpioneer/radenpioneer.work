390d58c6ebc0fa968b244cc8858490f78ccd204a
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-21 15:35:29 +0000

    Update Hero component title styling and content, and define theme colors in global CSS

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,7 +5,7 @@ const Hero: FC<{ title: string }> = ({ title }) => {
   return (
     <Section className='flex flex-col items-center justify-center select-none'>
       <div className='max-w-screen-lg mx-auto px-4'>
-        <h1 className='text-6xl md:text-8xl font-black uppercase text-center'>
+        <h1 className='text-6xl md:text-8xl font-black uppercase text-center text-theme-primary'>
           {title}
         </h1>
       </div>
