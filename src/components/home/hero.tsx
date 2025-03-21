1b3c3248e17178c609cb898d1fed395adc0761be
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-03-21 09:51:39 +0000

    Update Hero component to prevent text selection and remove unused .gitkeep file

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -3,7 +3,7 @@ import Section from './section'
 
 const Hero: FC<{ title: string }> = ({ title }) => {
   return (
-    <Section className='flex flex-col items-center justify-center'>
+    <Section className='flex flex-col items-center justify-center select-none'>
       <div className='max-w-screen-lg mx-auto px-4'>
         <h1 className='text-6xl md:text-8xl font-black uppercase text-center'>
           {title}
