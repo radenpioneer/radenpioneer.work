6223137ad0b7b5234afbcbda85c00e7663ea0c32
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-10-16 16:39:23 +0000

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
@@ -10,6 +9,6 @@ export default defineConfig({
       enabled: true
     },
 
-    imageService: 'cloudflare'
+    imageService: 'compile'
   })
 })
