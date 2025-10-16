6055ed6fa7f9ee7cfdfc8c1aaae2a42a50d6653b
R. Ilham Sastronegoro <bohrium.bearded_64@icloud.com>
2025-10-16 16:06:26 +0000

    RESET

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+
 import cloudflare from '@astrojs/cloudflare'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@astrojs/react'
-import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://radenpioneer.work',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
-    }
-  }),
-  integrations: [react(), keystatic()],
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: import.meta.env.PROD
-        ? {
-            'react-dom/server': 'react-dom/server.edge'
-          }
-        : {}
-    }
-  }
+    },
+
+    imageService: 'cloudflare'
+  })
 })
